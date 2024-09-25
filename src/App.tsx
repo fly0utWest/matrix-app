cd9220b35ed9163f2d3fceb224f41be38846e485
Nikita Ablamskiy <shealtov@gmail.com>
2024-09-25 18:21:12 +0300

    Template is ready

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Board from "./components/Board"
+
+const App: React.FC = () => {
+  return (
+    <main className="flex flex-1 p-5 flex-col h-full items-center justify-center bg-gray-300">
+    
+      <Board />
+    </main>
+  )
+}
+
+export default App
